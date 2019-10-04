a070ead59053480eec49c3ab3bd0e5f27531dae5
1612719 <danghuyentram@gmail.com>
2019-10-04 17:09:31 +0700

    all

---
+++ custom-app/src/test/java/com/example/customapp/CustomAppApplicationTests.java
@@ -0,0 +1,49 @@
+//package com.example.customapp;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import javax.validation.constraints.Null;
+//import java.util.Arrays;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//import static org.junit.Assert.assertNotNull;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class CustomAppApplicationTests {
+//
+//
+////	@Test
+////	public void checkExistDispatcherServlet(){
+////		ApplicationContext ctx = SpringApplication.run(CustomAppApplication.class);
+////		assertNotNull(ctx.getBean("dispatcherServlet"));
+////	}
+//
+////	@Test
+////	public void checkExistCustomHelloService(){
+////		ApplicationContext ctx = SpringApplication.run(CustomAppApplication.class);
+////		assertNotNull(ctx.getBean("customHelloService"));
+////	}
+//
+////
+////	@Test
+////	public void showBeanInContext(){
+////		ApplicationContext ctx = SpringApplication.run(CustomAppApplication.class);
+////		String[] beanNames = ctx.getBeanDefinitionNames();
+////		System.out.println("Bean list");
+////		Arrays.sort(beanNames);
+////		for (String beanName : beanNames) {
+////			System.out.println(beanName);
+////		}
+////	}
+////
+////
+//
+//
+//
+//}
